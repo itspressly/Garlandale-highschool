9766cca789f9d8ce1ac99c6a35b459acfd13c565
1nathi <120408540+1nathi@users.noreply.github.com>
2024-12-02 23:51:49 +0200

    Add files via upload

---
+++ Garlandale-highschool/Garlandale-highschool/index.js
@@ -0,0 +1,14 @@
+<script>
+    <html>
+        <body>
+            <nav class="navbor">
+                <ul class="nav-menu">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </body>
+    </html>
+</script>
---
+++ Garlandale-highschool/index.js
@@ -0,0 +1,13 @@
+<script>
+    <html>
+        <body>
+            <nav class="navbar"></nav>
+            <ul class="nav-menu">
+                <li><a href="#home"></a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </body>
+    </html>
+</script>
